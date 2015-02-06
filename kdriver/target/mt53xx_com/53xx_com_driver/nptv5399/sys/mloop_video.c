cb4183f76debf915ef7fb6db7d2d8ba973fdda9f
p4admin <p4admin@mediatek.com>
2015-02-06 09:08:08 +0800

    [MTWTV-1578][VIDEO][VSC]There is a transition video when change other input to Analog TV. .
    ---- mtk usage begin ---
    MTK_Submitter: jiangang.li
    MTK_MP_CL: 1918660
    MTK_RLS_CL: 1918671
    MTK_CR_ID: DTV00633590
    ---- mtk usage end ---
    .

--- kdriver/target/mt53xx_com/53xx_com_driver/nptv5399/sys/mloop_video.c
+++ kdriver/target/mt53xx_com/53xx_com_driver/nptv5399/sys/mloop_video.c
@@ -75,9 +75,9 @@
 /*-----------------------------------------------------------------------------
  *
  * $Author: p4admin $
- * $Date: 2015/01/30 $
+ * $Date: 2015/02/06 $
  * $RCSfile: mloop_video.c $
- * $Revision: #5 $
+ * $Revision: #6 $
  *
  *---------------------------------------------------------------------------*/
 
@@ -131,7 +131,6 @@
 #include "vdp_drvif.h"
 #include "vdp_display.h"
 #include "vdo_if.h"
-#include "vdp_if.h"
 #include "vdo_misc.h"
 #include "srm_drvif.h"
 #include "tve_if.h"
@@ -258,13 +257,6 @@ extern UINT8 _fgPostMuteInitStage;
 void vVdoModeChgDone(UINT8 bPath)
 {
     RVChannel *pt_rv_channel = (bPath == SV_VP_MAIN) ? (&_rMChannel) : (&_rPChannel);
-    VDP_REGION_T rSrcRegion;
-    
-    rSrcRegion.u4X = 0;
-    rSrcRegion.u4Y = 0;
-    rSrcRegion.u4Width = wDrvVideoInputWidth(bPath);
-    rSrcRegion.u4Height = wDrvVideoInputHeight(bPath);
-    VDP_SetSrcRegion(bPath,0, rSrcRegion);
 
     // Check video path characteristic
     pt_rv_channel->bIs444 = bIsScalerInput444(bPath);
