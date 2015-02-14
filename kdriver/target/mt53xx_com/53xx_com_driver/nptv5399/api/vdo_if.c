5a37d416df3156e06d241ecf89f8d8f17414894d
heng.cao <heng.cao@mediatek.com>
2015-02-14 23:18:06 +0800

    [DTV00634066] release branch [DTV00636790] [MTWTV-1653]MTWTV-1492 [AUDIO] No audio after changing HDMI -> TV with HDMI ARC

--- kdriver/target/mt53xx_com/53xx_com_driver/nptv5399/api/vdo_if.c
+++ kdriver/target/mt53xx_com/53xx_com_driver/nptv5399/api/vdo_if.c
@@ -74,10 +74,10 @@
  *---------------------------------------------------------------------------*/
 /*-----------------------------------------------------------------------------
  *
- * $Author: p4admin $
- * $Date: 2015/02/13 $
+ * $Author: heng.cao $
+ * $Date: 2015/02/14 $
  * $RCSfile: vdo_if.c,v $
- * $Revision: #34 $
+ * $Revision: #35 $
  *
  *---------------------------------------------------------------------------*/
 
@@ -845,11 +845,11 @@ UINT8 bApiVSCMainSubSrc(UINT8 bMainSrc, UINT8 bSubSrc, UINT8 u1SrcIdx)
 	
 	if(fgMainCh)
 	{
-	    if((bNewMainDec != SV_VD_DVI)&&(bNewMainDec != SV_VD_NA))
+	    /*if((bNewMainDec != SV_VD_DVI)&&(bNewMainDec != SV_VD_NA))
 	    {
            vDviConnect(SV_VP_MAIN, 0);
 		   LOG(2,"Vdo main call Disconnect DVI\n");
-		}
+		}*/
 		if(bNewMainDec == SV_VD_MPEGHD)
 		{
 			LG_PipLineVdpConnect(SV_VP_MAIN,u1SrcIdx);
@@ -870,11 +870,11 @@ UINT8 bApiVSCMainSubSrc(UINT8 bMainSrc, UINT8 bSubSrc, UINT8 u1SrcIdx)
 
 	if(fgPipCh)
 	{
-	    if((bNewSubDec != SV_VD_DVI)&&(bNewSubDec != SV_VD_NA))
+	   /* if((bNewSubDec != SV_VD_DVI)&&(bNewSubDec != SV_VD_NA))
 	    {
            vDviConnect(SV_VP_PIP, 0);
 		   LOG(2,"Vdo sub call Disconnect DVI\n");
-		}
+		}*/
 		if(bNewSubDec == SV_VD_MPEGHD)
 		{
 			LG_PipLineVdpConnect(SV_VP_PIP,u1SrcIdx);
