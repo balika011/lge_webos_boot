1108ff7382e7b6daacc2920062b0ff444356b826
p4admin <p4admin@mediatek.com>
2015-02-28 16:45:41 +0800

    [MTWTV-1682] [AUDIO] Sound Noise when SoundEngine on [Root cause] ARM11 (LGSE) DRAM priority is low and dram access timeout happens which causes audio output underrun and sound noise. [Solution] 1. Optimize LGSE MIPS and dram acess performance 2. increase ARM11 DRAM access priority.
    ---- mtk usage begin ---
    MTK_Submitter: jiuqiang.ming
    MTK_MP_CL: 1936202
    MTK_RLS_CL: 1936206
    MTK_CR_ID: DTV00636988
    ---- mtk usage end ---
    .

--- kdriver/target/mt5882/5882_driver/drv_cust/drv_default.h
+++ kdriver/target/mt5882/5882_driver/drv_cust/drv_default.h
@@ -142,7 +142,8 @@
 
 #ifndef DRAM_PRIORITY_LIST
 // Audio/0 > B2R/5 > VBI/3D/TVE/2 > SCPOS/7 > NR/PSCAN/4 > OSD/OD/MMU/3 > MJC_IN/13 > MJC_OUT/14 > DEMUX/GCPU/1 > CPU/6 > VLD1/9 > VDEC_MC/8 > Test0/15 > GFX/11 > VENC/12 > 3DGFX/10
-#define DRAM_PRIORITY_LIST          ((UINT8*)"08254193bafde67c")
+//#define DRAM_PRIORITY_LIST          ((UINT8*)"08254193bafde67c")
+#define DRAM_PRIORITY_LIST          ((UINT8*)"092651a3cbfde784")
 #endif
 
 #ifndef DRAM_BURSTLEN
